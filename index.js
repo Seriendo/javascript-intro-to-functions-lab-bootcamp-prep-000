9d6f258c0fbd759cc75fde51528322183a2b9756
Jonathan Nguyen <jnnj12@yahoo.com>
2018-07-02 22:36:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+ console.log(string.toUpperCase())
+  }
+  
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+ if (string.toLowerCase() === lowercase)
+  return ("I can't hear you!")
+ 
+}
+
+function sayHIToGrandma(string){
+ if (string.toUpperCase() === uppercase)
+ return ("YES INDEED!")
+}
+
+
+
+
+var uppercase = 'HELLO'
+var lowercase = 'hello'
